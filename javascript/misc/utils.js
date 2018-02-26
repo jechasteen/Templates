49553687ab29fb531ab32684b6bcc76ec08122f7
Jay Chasteen <jonathan.chasteen@live.com>
2018-02-26 17:56:13 -0500

    refactor and add funcTimer
    funcTimer is extracted from the "100 Days of Code" repository in preparation for deleting it.

--- javascript/misc/utils.js
+++ javascript/misc/utils.js
@@ -1,16 +1,32 @@
 'use strict'
-var utils = {}
+var utils = {
+  host:
+  function (ip) {
+    if (ip === '127.0.0.1') return 'localhost'
+    else return ip
+  },
 
-var utils.host = function (ip) {
-  if (ip === '127.0.0.1') return 'localhost'
-  else return ip
-}
+  isLoggedIn:
+  function(req, res, next) {
+    if (req.isAuthenticated()) {
+      next()
+    } else {
+      res.redirect('back')
+    }
+  },
 
-var utils.isLoggedIn = function(req, res, next) {
-  if (req.isAuthenticated()) {
-    next()
-  } else {
-    res.redirect('back')
+  funcTimer:
+  function(boolReturn, callback) {  
+    var t0 = new Date().getTime();
+    if (!boolReturn){
+      callback();
+      var t1 = new Date().getTime();
+    } else {
+      var fromFuncReturn = callback();
+      var t1 = new Date().getTime();
+    }
+    console.log( "Function took " + (t1 - t0) + " milliseconds." );
+    return fromFuncReturn;
   }
 }
 
